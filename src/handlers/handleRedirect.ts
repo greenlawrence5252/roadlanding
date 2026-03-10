5bb05c14e5acd58330f0691e0e0049395ad370a7
dngoctai132-cyber <dngoctai132@gmail.com>
2026-03-10 16:17:27 +0300

    dsadasdsadddd

--- src/handlers/handleRedirect.ts
+++ src/handlers/handleRedirect.ts
@@ -2,7 +2,7 @@ export default function handleRedirect() {
   const botUsername = import.meta.env.VITE_BOT_USERNAME
   const startParam = import.meta.env.VITE_START_PARAM
   const url = startParam
-    ? `tg://resolve?domain=${botUsername}&start=${startParam}`
-    : `tg://resolve?domain=${botUsername}`
+    ? `https://t.me/${botUsername}?start=${startParam}`
+    : `https://t.me/${botUsername}`
   window.location.href = url
 }
